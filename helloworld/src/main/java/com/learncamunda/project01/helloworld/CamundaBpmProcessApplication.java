1fd9002d07cb8435cde402a79ee7a0cd58a2b5c3
Sandy <sandycodesearch@gmail.com>
2021-01-03 16:55:52 +0530

    first commit

---
+++ helloworld/src/main/java/com/learncamunda/project01/helloworld/CamundaBpmProcessApplication.java
@@ -0,0 +1,12 @@
+package com.learncamunda.project01.helloworld;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+/**
+ * Process Application exposing this application's resources to the process engine.
+ */
+@ProcessApplication
+public class CamundaBpmProcessApplication extends ServletProcessApplication {
+
+}
---
+++ helloworld/src/main/java/com/learncamunda/project01/helloworld/ProcessConstants.java
@@ -0,0 +1,7 @@
+package com.learncamunda.project01.helloworld;
+
+public class ProcessConstants {
+
+  public static final String PROCESS_DEFINITION_KEY = "helloworld"; // BPMN Process ID
+
+}
